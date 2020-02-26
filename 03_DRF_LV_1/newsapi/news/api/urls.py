a8588bd6142aab36c9e5ee9cfa838eb6d102d8a0
pasqualespica <pasqualespica>
2020-02-26 12:34:39 +0100

    remove func and replace with APIView baseon ClassBaseView

--- 03_DRF_LV_1/newsapi/news/api/urls.py
+++ 03_DRF_LV_1/newsapi/news/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from news.api.views import article_list_create_api_view, article_detail_api_view
-
+# from news.api.views import article_list_create_api_view, article_detail_api_view
+from news.api.views import ArticleListCreateAPIview, ArticleDetailAPIview
 
 # here define ENDPOINT ...
-
 urlpatterns = [
-    path('articles/', article_list_create_api_view, name="article-list"),
-    path('articles/<int:pk>', article_detail_api_view, name="article-detail")
+    path('articles/', ArticleListCreateAPIview.as_view(), name="article-list"),
+    path('articles/<int:pk>', ArticleDetailAPIview.as_view(), name="article-detail")
+    # path('articles/', article_list_create_api_view, name="article-list"),
+    # path('articles/<int:pk>', article_detail_api_view, name="article-detail")
 ]
--- 03_DRF_LV_1/newsapi/news/api/views.py
+++ 03_DRF_LV_1/newsapi/news/api/views.py
@@ -1,5 +1,10 @@
+# shortcut di Djangio per gestire in atuomatico Try/Except 
+from django.shortcuts import get_object_or_404
+
 from rest_framework import status
 from rest_framework.decorators import api_view
+# importiamo APIView - relatedTo - CLassBaseView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from news.models import Article
@@ -28,46 +33,95 @@ di API View:
 #  PUT / PATCH     : aggiornare una risorsa
 #  DELETE          : cancellare una risorsa
 
-@api_view(["GET", "POST"]) # Read and Create
-def article_list_create_api_view(request):
-    if request.method == "GET" :
+class ArticleListCreateAPIview(APIView):
+    """
+    Mostra una elenco degli articoli e ne crea di nuovi !!!
+    a differenza della function view non utlizzeremo 'if' per capire il metodo
+    ma invece definiremo dei metodi
+    """
+    def get(self, request):
         articles = Article.objects.filter(active=True)
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST" :
+    def post(self, request):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
-@api_view(["GET", "PUT", "DELETE"])
-def article_detail_api_view(request, pk):
+# @api_view(["GET", "POST"]) # Read and Create
+# def article_list_create_api_view(request):
+#     if request.method == "GET" :
+#         articles = Article.objects.filter(active=True)
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
 
-    try :
-        article = Article.objects.get(pk=pk)
-    except Article.DoesNotExist:
-        return Response({"Error" :{
-            "code" : 404,
-            "message" : "articolo non trovato"
-        }},
-        status=status.HTTP_404_NOT_FOUND)
+#     elif request.method == "POST" :
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
-    if request.method == "GET" : # read
+
+
+class ArticleDetailAPIview(APIView):
+    """
+    retrieve, update, delete per un'instanza di Article
+    """
+
+    def get_object(self, pk):
+        article = get_object_or_404(Article, pk=pk)
+        return article
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
 
-    elif request.method == "PUT" : # update
+    def put(self, request, pk):
+        article = self.get_object(pk)
         serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         # se non valido
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == "DELETE" :
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(["GET", "PUT", "DELETE"])
+# def article_detail_api_view(request, pk):
+
+#     try :
+#         article = Article.objects.get(pk=pk)
+#     except Article.DoesNotExist:
+#         return Response({"Error" :{
+#             "code" : 404,
+#             "message" : "articolo non trovato"
+#         }},
+#         status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == "GET" : # read
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+
+#     elif request.method == "PUT" : # update
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         # se non valido
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     elif request.method == "DELETE" :
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
     
