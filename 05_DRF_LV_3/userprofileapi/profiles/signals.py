5e83c623854b19751efb1f5bb380df334b1e8d3b
pasqualespica <pasqualespica>
2020-03-06 15:11:38 +0100

    Add User Profile - SIGNALS

---
+++ 05_DRF_LV_3/userprofileapi/profiles/signals.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save # User mandera' il seganle post_save ogni volta che viene salvata
+from django.dispatch import receiver # decoratore per ricevere uesto seganale
+from profiles.models import Profile # crere un nuovo profile
+
+
+# Per poterla utlizzare va fatta una configurazione in 
+# __init__.py
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    print("Created ...", created)
+    if created:
+        Profile.objects.create(user=instance)
+
