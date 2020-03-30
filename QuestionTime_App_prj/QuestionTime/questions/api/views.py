022331e78693304e05ddd35dc7ac6595b0de18d5
pasqualespica <pasqualespica>
2020-03-30 19:19:05 +0200

    add Like ( mi piace ) detailview

--- QuestionTime_App_prj/QuestionTime/questions/api/views.py
+++ QuestionTime_App_prj/QuestionTime/questions/api/views.py
@@ -4,6 +4,10 @@ from rest_framework import generics, viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
 from questions.api.serializers import AnswerSerializer, QuestionSerializer
 from questions.models import Answer, Question
 
@@ -44,3 +48,38 @@ class QuestionAnswerListAPIView(generics.ListAPIView):
         # https://docs.djangoproject.com/en/3.0/topics/db/queries/
         # django Field lookups
         return Answer.objects.filter(question__slug=kwarg_slug)
+
+# retrive - update - delete
+class AnswerRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+# endpoint per i mi-piace - POST and DELETE 
+class AnswerLikeAPIView(APIView):
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        answer = get_object_or_404(Answer, pk=pk)
+        user = self.request.user
+
+        answer.voters.remove(user)
+        answer.save()
+
+        serializer_context = { "request" : request }
+        serializer = self.serializer_class(answer, context= serializer_context)
+
+        return Response(serializer.data, status= status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        answer = get_object_or_404(Answer, pk=pk)
+        user = self.request.user
+
+        answer.voters.add(user)
+        answer.save()
+
+        serializer_context = { "request" : request }
+        serializer = self.serializer_class(answer, context= serializer_context)
+
+        return Response(serializer.data, status= status.HTTP_200_OK)
